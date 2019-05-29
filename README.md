39e1ea1208fa24e16d93354fc88e3b75eb2fa852
alex.marques <230681a1>
2019-05-29 17:00:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_agenda_contatos
+
+Aplicativo de controle de contatos
+
+## Iniciando projeto
+
+Esse projeto é um aplicativo de controle de contatos
---
+++ lib/helpers/contact_helper.dart
@@ -0,0 +1,20 @@
+import 'package:sqflite/sqflite.dart';
+
+class ContactHelper{
+
+}
+
+// id name email phone img
+
+class Contact {
+
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  Contact.fromMap(){
+
+  }
+}
